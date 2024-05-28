16e0e1fef42c7000eb54f841b87e8ef1d672259b
ansorokey <ansorokey@gmail.com>
2024-05-28 15:06:23 -0400

    setting up clerk

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+// This is the root home page
+// Only logged in users should be able to see this page
+
+export default function Home() {
+  return (
+    <p>This is an authenticated route</p>
+  );
+}
