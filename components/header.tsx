6b45fe6abfb89f9ede75d79242d11e18a7421c81
ansorokey <ansorokey@gmail.com>
2024-06-03 14:45:55 -0400

    basic logo layout

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,15 @@
+import { Logo } from "./logo";
+
 export const Header = () => {
     return (
         <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
-            Header Component
+            <div className="max-w-screen-2xl mx-auto">
+                <div className="w-full flex items-center justify-between mb-14">
+                    <div className="flex items-center lg:gap-x-16">
+                        <Logo />
+                    </div>
+                </div>
+            </div>
         </header>
     );
 };
