896628f70875590547bd20456ecff1b651db4822
ansorokey <ansorokey@gmail.com>
2024-06-03 14:41:58 -0400

    some styling

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
-        <div>
+        <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
             Header Component
-        </div>
+        </header>
     );
 };
