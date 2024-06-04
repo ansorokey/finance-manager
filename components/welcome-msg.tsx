0e7f1c655ec1fe1112f40f8dc208c6f800fcbf5c
ansorokey <ansorokey@gmail.com>
2024-06-04 13:08:38 -0400

    header styled, added user first name

--- components/welcome-msg.tsx
+++ components/welcome-msg.tsx
@@ -6,11 +6,11 @@ export const WelcomeMsg = () => {
     const { user, isLoaded } = useUser();
 
     return (
-        <div>
-            <h2>
-                Welcome Back
+        <div className="space-y-2 mb-4">
+            <h2 className="text-2xl lg:text-4xl text-white font-medium ">
+                Welcome Back {isLoaded ? ", " : " "}{user?.firstName}👋
             </h2>
-            <p>
+            <p className="text-sm lg:text-base text-[#89b6fd]">
                 This is your Financial Overview Report
             </p>
         </div>
