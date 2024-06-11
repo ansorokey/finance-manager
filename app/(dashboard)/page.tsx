dd3d33c34ad75d2ba7c6308c4c47add66ce5c00f
ansorokey <ansorokey@gmail.com>
2024-06-11 09:58:29 -0400

    simplified error handling

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-
 // This is the root home page
 // Only logged in users should be able to see this page
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,20 +1,12 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from "./accounts";
-import { HTTPException } from 'hono/http-exception';
-import { error } from 'console';
 
 // Axios (used by plaid) does not work well with edge
 export const runtime = 'edge';
 
 // All routes are prepended with /api
 const app = new Hono().basePath('/api');
-
-app.onError((err, c) => {
-    if (err instanceof HTTPException) {
-        return error.getResponse();
-    }
-});
 
 // all sub routes must be chained in order to export
 const routes = app
