c40cd259e6dbe40eac5589f7cd206b29f94b848c
ansorokey <ansorokey@gmail.com>
2024-06-12 09:59:30 -0400

    adding cuid for account creation

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
+import { zValidator } from "@hono/zod-validator";
 
 const app = new Hono()
     .get('/', clerkMiddleware(), async (c) => {
@@ -20,6 +21,24 @@ const app = new Hono()
         .where(eq(accounts.userId, auth.userId));
 
         return c.json({ data });
+    })
+    .post('/', zValidator("json", insertAccountSchema.pick({
+        name: true,
+    })), clerkMiddleware(), async (c) => {
+        const auth = getAuth(c);
+        const values = c.req.valid("json");
+
+        if (!auth?.userId) {
+            return c.json({ error: "Unauthorized"}, 401);
+        }
+
+        const data = await db.insert(accounts).values({
+            id: "test",
+            userId: auth.userId,
+            ...values
+        });
+
+        return c.json({});
     });
 
 export default app;
